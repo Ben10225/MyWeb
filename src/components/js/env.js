9ebe8df27f0103a01ae16454324629e24e59e7ae
pon <pon@ponde-MacBook-Pro.local>
2023-05-26 23:20:19 +0800

    modify

---
+++ src/components/js/env.js
@@ -0,0 +1,11 @@
+// const SERVICE_ID = "service_1k6n04x";
+// const TEMPLATE_ID = "template_6at1uj7";
+// const PUBLIc_KEY = "sbkxdSm9ZjUBrgerk";
+
+const emailAccount = {
+  SERVICE_ID: "service_1k6n04x",
+  TEMPLATE_ID: "template_6at1uj7",
+  PUBLIC_KEY: "sbkxdSm9ZjUBrgerk",
+};
+
+export default emailAccount;
